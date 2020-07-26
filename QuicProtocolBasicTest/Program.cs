2ab7d288a0332db909492cacf5b539859b0a5086
P1561 <P1561@DESKTOP-0E8MCVH>
2020-07-26 12:12:28 +0300

    initial

---
+++ QuicProtocolBasicTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using QuicNet;
+using QuicNet.Connections;
+using System;
+using System.Text;
+
+namespace QuicProtocolBasicTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server");
+            QuicListener listener = new QuicListener(18000);
+            listener.Start();
+            while (true)
+            {
+                // Blocks while waiting for a connection
+                QuicConnection client = listener.AcceptQuicClient();
+
+                // Assign an action when a data is received from that client.
+                client.OnDataReceived += (c) => {
+                    byte[] data = c.Data;
+                    Console.WriteLine("Data received: " + Encoding.UTF8.GetString(data));
+                    // Echo back data to the client
+                    c.Send(Encoding.UTF8.GetBytes("Echo!"));
+                };
+            }
+        }
+    }
+}
---
+++ QuicProtocolBasicTestClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using QuicNet;
+using QuicNet.Connections;
+using QuicNet.Streams;
+using System;
+using System.Text;
+
+namespace QuicProtocolBasicTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client");
+            QuicClient client = new QuicClient();
+            QuicConnection connection = client.Connect("127.0.0.1", 18000);   // Connect to peer (Server)
+
+            // Create a Bidirectional data stream
+            QuicStream stream = connection.CreateStream(QuickNet.Utilities.StreamType.ClientBidirectional);
+
+            // Send Data
+            var message = Console.ReadLine();
+            stream.Send(Encoding.UTF8.GetBytes(message));
+
+            // Receive from server (Blocks)
+            byte[] data = stream.Receive();
+            Console.ReadKey();
+        }
+    }
+}
